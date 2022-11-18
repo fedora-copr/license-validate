fc79d4785909ad8444cff1ea71ff2af4a1180501
Sandro <devel@penguinpee.nl>
2022-11-18 15:13:58 +0000

    Fix typo in license-invalid-as-spdx.sh

--- license-invalid-as-spdx.sh
+++ license-invalid-as-spdx.sh
@@ -6,14 +6,14 @@ LICENSE=$(grep '^License:' "rpm-specs/${SPEC}.spec" | cut '-d:' -f2- )
 
 if license-validate --old "$LICENSE" >/dev/null;  then
         if license-validate "$LICENSE" >/dev/null; then
-                echo "$1 warning: valid as old and new and now changelong entry, please check"
+                echo "$1 warning: valid as old and new and no changelong entry, please check"
         else
                 if license-fedora2spdx "$LICENSE" | grep Warning >/dev/null; then
                         # this is not straight forward conversion
                         echo "$1"
                 else
                         SPDXLICENSE=$(license-fedora2spdx "$LICENSE")
-                        echo "$1 - can be trivialy converted to $SPDXLICENSE"
+                        echo "$1 - can be trivially converted to $SPDXLICENSE"
                 fi
         fi
 else
