f8402dbf2bcb8895a0f474a5d3ef31c349f0603c
Miroslav Suchý <msuchy@redhat.com>
2023-02-09 15:30:07 +0100

    generate list of maintainers

--- complete-check.sh
+++ complete-check.sh
@@ -9,3 +9,6 @@
 grep -v '^rubygem-' |grep -v '^rust-' | tee packages-without-spdx-in-spec-changelog-grepped.txt | \
 ./packages-without-spdx-in-distgit-changelog.sh | tee ./packages-without-spdx-in-distgit-changelog.txt | \
 ./packages-with-invalid-license.sh > packages-without-spdx-final.txt
+
+cat final_just_packages.txt |sort | uniq > /tmp/final_just_packages.txt
+find-package-maintainers /tmp/final_just_packages.txt > packages-without-spdx-final-maintainers.txt
--- license-invalid-as-spdx.sh
+++ license-invalid-as-spdx.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 
 SPEC="$1"
+FILENAME_ALL_PACKAGES="final_just_packages.txt"
+POSITIVE=""
 
 grep '^License:' "rpm-specs/${SPEC}.spec" | cut '-d:' -f2- | \
 while read -r LICENSE; do 
@@ -16,12 +18,13 @@ while read -r LICENSE; do
                         echo "$1 - can be trivially converted to $SPDXLICENSE"
                 fi
         fi
+        echo "${SPEC}" >> $FILENAME_ALL_PACKAGES
 else
         if license-validate "$LICENSE" >/dev/null; then
                 true;
         else
                 echo "$1 warning: not valid neither as Callaway nor as SPDX, please check"
+                echo "${SPEC}" >> $FILENAME_ALL_PACKAGES
         fi
   fi
 done
-
