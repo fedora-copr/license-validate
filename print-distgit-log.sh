1f94ca9294ad2786a061043d5da1f4696def0af7
Miroslav Suchý <msuchy@redhat.com>
2023-03-10 14:21:53 +0100

    another fix

--- print-distgit-log.sh
+++ print-distgit-log.sh
@@ -2,9 +2,11 @@
 
 test -d ~/spdx || mkdir ~/spdx/
 
+DIR=${PWD}
+
 pushd ~/spdx/ >/dev/null
 
 # the leading dot is important otherwise the change dir will not work
-. ./fedpkg-clone-and-cd-package.sh "$1"
+. ${DIR}/fedpkg-clone-and-cd-package.sh "$1"
 git log --pretty=format:%s
 popd >/dev/null
