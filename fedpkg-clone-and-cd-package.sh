ed2a5736cc03d293a88627336cccb4052a4f1bc1
Miroslav Suchý <msuchy@redhat.com>
2023-03-10 14:34:44 +0100

    another fix

--- fedpkg-clone-and-cd-package.sh
+++ fedpkg-clone-and-cd-package.sh
@@ -2,8 +2,8 @@
 
 if [ -d "$1" ]; then
         cd "$1"
-        git reset --hard HEAD
-        timeout 60 fedpkg pull
+        git reset --hard HEAD >/dev/null
+        timeout 60 fedpkg pull -- -q  >/dev/null 
 else
         timeout 60 fedpkg clone -a "$1" -- -q >/dev/null
         cd "$1"
