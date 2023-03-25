ed72a78d57acbaaa1426e689c131bffb47d696ac
Miroslav Suchý <msuchy@redhat.com>
2023-03-25 10:53:07 +0100

    fix

--- complete-check.sh
+++ complete-check.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 rm -rf rpm-specs/*
 rm -f final_just_packages.txt
+rm -f eln-list.txt
+
+wget https://tiny.distro.builders/view-all-source-package-name-list--view-eln.txt -O eln-list.txt
 
 # creates package-license.txt and license.txt
 ./download-all-fedora-licenses
@@ -10,7 +13,7 @@ rm -f final_just_packages.txt
 # ignore packages reported by maintainers that are ok, but do not have {spec,git}log entry
 ./ignore-packages.py ignore-packages.txt | tee packages-without-spdx-in-spec-changelog-grepped.txt | \
 # ignore packages that has been already migrated
-./ignore-packages.py already-migrated-packages.txt | tee packages-has-not-been-migrated.txt | \
+#./ignore-packages.py already-migrated-packages.txt | tee packages-has-not-been-migrated.txt | \
 ./packages-without-spdx-in-distgit-changelog.sh | tee ./packages-without-spdx-in-distgit-changelog.txt | \
 ./packages-before-spdx-was-standard.sh | tee ./packages-before-spdx-was-standard.txt | \
 ./packages-with-invalid-license.sh > packages-without-spdx-final.txt
