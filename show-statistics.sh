c2ea5fbca2a59e604c1f8a45cfc196a0a39f7c0c
Miroslav Suchý <msuchy@redhat.com>
2023-04-08 13:46:40 +0200

    update statistics

--- show-statistics.sh
+++ show-statistics.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-echo -n "License Tags"
-wc -l package-license.txt
+echo -n "License Tags: "
+wc -l < package-license.txt
 
-echo -n "Not converted yet"
-wc -l packages-without-spdx-final.txt
+echo -n "Not converted yet: "
+wc -l < packages-without-spdx-final.txt
 
 echo -n "Trivial conversion: "
 grep trivial packages-without-spdx-final.txt |wc -l
