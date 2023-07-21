e75d53f6109687ea3022e29161ae14071f0fd05f
Miroslav Suchý <msuchy@redhat.com>
2023-07-21 15:14:00 +0200

    add debug prints

--- fedpkg-clone-and-cd-package.sh
+++ fedpkg-clone-and-cd-package.sh
@@ -3,8 +3,8 @@
 if [ -d "$1" ]; then
         cd "$1"
         git reset --hard HEAD >/dev/null
-        timeout 60 fedpkg pull -- -q  >/dev/null 
+        timeout 60 fedpkg pull -- -q  >/dev/null || echo "Error pulling '$1'"
 else
-        timeout 60 fedpkg clone -a "$1" -- -q >/dev/null
+        timeout 60 fedpkg clone -a "$1" -- -q >/dev/null || echo "Error clonnig '$1'"
         cd "$1"
 fi
