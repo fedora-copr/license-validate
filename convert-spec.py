da7ee888cf23a68eaeaa68fe116e962851254d6e
Miroslav Suchý <msuchy@redhat.com>
2024-04-05 10:50:25 +0200

    add tooling for mass changes

--- convert-spec.py
+++ convert-spec.py
@@ -2,19 +2,22 @@
 from specfile import Specfile
 import subprocess
 import sys
+from datetime import datetime
 
-
-def alter_license(tags):
-    data = subprocess.Popen(['license-fedora2spdx', tags.license.value], stdout = subprocess.PIPE)
-    output = data.communicate()
+def alter_license(license):
+    data = subprocess.Popen(['license-fedora2spdx', license], stdout = subprocess.PIPE)
+    output = data.communicate()[0].strip().decode()
     if "Warning" in str(output[0]):
         sys.stderr.write("Error: Cannot convert automatically.")
         sys.exit(-1)
-    print(tags.license.value)
-    print("DBG" , output)
+    #print(license)
+    #print("DBG" , output)
+    return output
 
 # this is dump, but do the work
 filename = sys.argv[1]
+old_license = sys.argv[2]
+new_license = alter_license(old_license)
 specfile = Specfile(filename)
 
 with specfile.sections() as sections:
@@ -22,7 +25,18 @@ with specfile.sections() as sections:
         if section.name.startswith("package"):
             with specfile.tags(section) as tags:
                 if 'License' in tags:
-                    alter_license(tags)
+                    license = tags.license.value
+                    if license == old_license:
+                        tags.license.value = new_license
                     #print(section.name, tags.license.value)
                     #tags.license = "MIT"
+if not specfile.has_autorelease:
+    specfile.release = str(int(specfile.expanded_release) + 1)
+if not specfile.has_autochangelog:
+    specfile.add_changelog_entry(
+        f"- convert license to SPDX",
+        author='Miroslav Suchý',
+        email='msuchy@redhat.com',
+        timestamp=datetime.now().date(),
+    )
 specfile.save()
