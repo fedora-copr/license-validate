8bcc26b83a6927eed45ee72d0fb1324b044ee543
Miroslav Suchý <msuchy@redhat.com>
2024-09-08 10:12:43 +0200

    add print-spec-license.py

---
+++ print-spec-license.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import logging
+import sys
+from specfile import Specfile
+
+logger = logging.getLogger('specfile.spec_parser')
+logger.setLevel(logging.CRITICAL)
+
+# this is dump, but do the work
+filename = sys.argv[1]
+specfile = Specfile(filename, force_parse=True)
+
+with specfile.sections() as sections:
+    for section in sections:
+        if section.name.startswith("package"):
+            with specfile.tags(section) as tags:
+                if 'License' in tags:
+                    license = tags.license.value
+                    print(license)
