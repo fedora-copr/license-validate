dfc3e899153fb7b0c292feed5b9de7d67ca1e990
Miroslav Suchý <msuchy@redhat.com>
2024-10-23 09:23:01 +0200

    update statistics

--- show-statistics.sh
+++ show-statistics.sh
@@ -6,8 +6,11 @@ ls rpm-specs/*spec |wc -l
 echo -n "License Tags: "
 wc -l < package-license.txt
 
+echo -n "Not SPDX complient: "
+grep -v '^$' < packages-without-spdx-final.txt | grep -v "valid as old and new and no changelong entry, please check" | grep -v "contains LicenseRef-Callaway, needs to be manually converted" | wc -l
+
 echo -n "Not converted yet: "
-grep -v '^$' < packages-without-spdx-final.txt | wc -l
+grep -v '^$' < packages-without-spdx-final.txt | grep -v "valid as old and new and no changelong entry, please check"  | wc -l
 
 echo -n "Trivial conversion: "
 grep trivial packages-without-spdx-final.txt |wc -l
