bc374b45b331d99ab7712042605ce894670e2935
Miroslav Suchý <msuchy@redhat.com>
2026-01-23 08:48:19 +0100

    update statistics

--- generate-done-list.sh
+++ generate-done-list.sh
@@ -2,7 +2,7 @@
 
 ls rpm-specs/*spec | xargs basename -s .spec > all-packages.txt
 
-cat packages-without-spdx-final.txt |awk '{print $1 }' > not-migrated-packages.txt
+cat packages-without-spdx-final.txt | grep -v "valid as old and new and no changelong entry, please check" | awk '{print $1 }' > not-migrated-packages.txt
 
 # Use grep to find lines in file1 that are not in file2
 grep -Fxv -f "not-migrated-packages.txt" "all-packages.txt" > already-migrated-packages.txt
